850c9f4fe74bd8f80359e6a0c0945cddc5b84b96
NA-Silk <nicolasseda@gmail.com>
2024-10-29 03:08:02 -0400

    vite update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/shoppingreact",
-  base: "/vite-react-deploy/",
+  // base: "/vite-react-deploy/",
+  base: "/e-plantShopping",
   plugins: [react()],
 });
